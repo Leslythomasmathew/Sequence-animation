1ca5994301df46bc0640942574f23036a3941fe0
Leslythomasmathew <Leslymathew1306@gmail.com>
2026-06-18 20:11:47 +0530

    Update final.js

--- final.js
+++ final.js
@@ -1,33 +1,36 @@
-const box = document.getElementById("box");
-const text = document.getElementById("text");
+const box1 = document.getElementById("box1");
+const box2 = document.getElementById("box2");
+const box3 = document.getElementById("box3");
+
+const animation = [
+    {
+        transform: "translateY(0) scale(1)",
+        opacity: 1
+    },
+    {
+        transform: "translateY(-150px) scale(0)",
+        opacity: 0
+    }
+];
+
+const timing = {
+    duration: 1500,
+    fill: "forwards",
+    easing: "ease-in-out"
+};
 
 async function startAnimation() {
 
-    await box.animate(
-        [
-            {
-                transform: "scale(1) rotate(0deg)",
-                opacity: 1
-            },
-            {
-                transform: "scale(0) rotate(720deg)",
-                opacity: 0
-            }
-        ],
-        {
-            duration: 2500,
-            fill: "forwards",
-            easing: "ease-in-out"
-        }
-    ).finished;
+    await box1.animate(animation, timing).finished;
+    box1.style.display = "none";
 
-    box.style.display = "none";
+    await box2.animate(animation, timing).finished;
+    box2.style.display = "none";
+
+    await box3.animate(animation, timing).finished;
+    box3.style.display = "none";
 
     document.body.style.backgroundColor = "black";
-
-    setTimeout(() => {
-        text.style.opacity = "1";
-    }, 1000);
 }
 
 startAnimation();
